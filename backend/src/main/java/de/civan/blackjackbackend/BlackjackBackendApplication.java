7fa49915597db5cd6af87db482c9731ccaf4051e
Civan Erbay <Civan.Erbay@web.de>
2020-07-02 17:08:26 +0200

    Add Backend

---
+++ backend/src/main/java/de/civan/blackjackbackend/BlackjackBackendApplication.java
@@ -0,0 +1,13 @@
+package de.civan.blackjackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackjackBackendApplication.class, args);
+    }
+
+}
